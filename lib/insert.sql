df30430bd4569479cd027656392c7c9aad9b97d6
TheOrangeDot <theorangedottwitch@gmail.com>
2022-01-13 18:16:03 +0900

    Completed

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO 
+bears(name, age, sex, color, temperament, alive)
+VALUES("Mr. Chocolate", 15, "M", "Brown", "Aggressive", 1),
+("ROWDY", 2, "M", "Brown", "Rowdy", 1),
+("TABITHA", 5, "F", "Black", "Calm", 1),
+("Sergent Brown", 10, "M", "Black", "Aggressive", 0),
+("Melissa", 8, "F", "Black and White", "Lazy", 1) ,
+("Grinch", 1, "M", "Black and White", "Friendly", 1),
+("Wendy", 4, "F", "White", "Lazy", 1),
+(NULL, 0, "M", "White", NULL, 1);
